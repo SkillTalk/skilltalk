894d37425f9dc145be6dd8b218d725bf65b27978
SkillTalk <skilltalk55@gmail.com>
2025-02-27 13:49:49 +0530

    fix for mobile

--- server.js
+++ server.js
@@ -101,6 +101,10 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () =>
   console.log(`🚀 Server running at http://localhost:${PORT}`)
